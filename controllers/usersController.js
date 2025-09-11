b4081b35573ac7f3eccad818dfd7dfb13aaee53b
Salwa-mansour <alqadasisal@gmail.com>
2025-09-11 10:09:04 +0300

    failed

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,23 @@
-// controllers/usersController.js
 const usersStorage = require("../storages/usersStorage");
 
+// This just shows the new stuff we're adding to the existing contents
+const { body, validationResult } = require("express-validator");
+
+const alphaErr = "must only contain letters.";
+const lengthErr = "must be between 1 and 10 characters.";
+const validateUser = [
+  body("firstName").trim()
+    .isAlpha().withMessage(`First name ${alphaErr}`)
+    .isLength({ min: 1, max: 10 }).withMessage(`First name ${lengthErr}`),
+  body("lastName").trim()
+    .isAlpha().withMessage(`Last name ${alphaErr}`)
+    .isLength({ min: 1, max: 10 }).withMessage(`Last name ${lengthErr}`),
+];
+exports.textValidatore = function(){
+  return [body("firstName").trim()
+    .isAlpha().withMessage(`First name ${alphaErr}`)
+    .isLength({ min: 1, max: 10 }).withMessage(`First name ${lengthErr}`)]
+}
 exports.usersListGet = (req, res) => {
   res.render("index", {
     title: "User list",
@@ -15,6 +32,14 @@ exports.usersCreateGet = (req, res) => {
 };
 
 exports.usersCreatePost = (req, res) => {
+  this.textValidatore();
+   const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("createUser", {
+        title: "Create user",
+        errors: errors.array(),
+      });
+    }
   const { firstName, lastName } = req.body;
   usersStorage.addUser({ firstName, lastName });
   res.redirect("/");
