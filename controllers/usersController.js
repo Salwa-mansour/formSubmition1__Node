c3e61fe41d4f5b79ad35d04b8b9633ae05e1dc1e
Salwa-mansour <alqadasisal@gmail.com>
2025-09-13 07:18:12 +0300

    remove validation for now

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,18 +3,9 @@ const usersStorage = require("../storages/usersStorage");
 // This just shows the new stuff we're adding to the existing contents
 const { body, validationResult } = require("express-validator");
 
-const alphaErr = "must only contain letters.";
-const lengthErr = "must be between 1 and 10 characters.";
-const validateUser = [
-  body("firstName").trim()
-    .isAlpha().withMessage(`First name ${alphaErr}`)
-    .isLength({ min: 1, max: 10 }).withMessage(`First name ${lengthErr}`),
-  body("lastName").trim()
-    .isAlpha().withMessage(`Last name ${alphaErr}`)
-    .isLength({ min: 1, max: 10 }).withMessage(`Last name ${lengthErr}`),
-];
-exports.textValidatore = function(){
-  return [body("firstName").trim()
+
+exports.textValidatore = function(item){
+  return [body(item).trim()
     .isAlpha().withMessage(`First name ${alphaErr}`)
     .isLength({ min: 1, max: 10 }).withMessage(`First name ${lengthErr}`)]
 }
@@ -32,14 +23,15 @@ exports.usersCreateGet = (req, res) => {
 };
 
 exports.usersCreatePost = (req, res) => {
-  this.textValidatore();
-   const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).render("createUser", {
-        title: "Create user",
-        errors: errors.array(),
-      });
-    }
+
+  //  const errors =  this.textValidatore(req.body.firstName);
+  //  console.log(errors)
+  //   if (!errors.isEmpty()) {
+  //     return res.status(400).render("createUser", {
+  //       title: "Create user",
+  //       errors: errors.array(),
+  //     });
+  //   }
   const { firstName, lastName } = req.body;
   usersStorage.addUser({ firstName, lastName });
   res.redirect("/");
